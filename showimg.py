97742707a3da856226a86213d6178fa59f1d5611
pugsatoshi <satoshi.ito.dev@gmail.com>
2021-09-09 16:21:37 +0900

    first push

---
+++ showimg.py
@@ -0,0 +1,11 @@
+import numpy
+import cv2
+
+
+img = cv2.imread("test.jpg")
+img2 = cv2.imread("test.jpg",0)#グレースケールで読み込み
+
+cv2.imshow("color",img)
+cv2.imshow("gray",img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
